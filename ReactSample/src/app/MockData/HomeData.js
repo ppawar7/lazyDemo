dfaa5572c31e7dd33fb10f1ae1f7fea75e3e51b3
PUNAM\Home <punampawar23@gmail.com>
2018-08-25 00:29:08 +0530

    'react2'

---
+++ ReactSample/src/app/MockData/HomeData.js
@@ -0,0 +1,22 @@
+const homeMockData = [
+    {
+        imageUrl: 'https://mdbootstrap.com/img/Photos/Others/images/58.jpg',
+        headerText: 'Thought Leadership',
+        subHeaderText: 'How do you modernize your traditional systems to keep the day to day operations running smoothly in the Telecom Industry in order to make time for innovation. Check out Gnanapriya, Telecom expert, Infosys explain in her blog.',
+        knowMoreText: 'VIEW'
+    },
+    {
+        imageUrl: 'https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Products/img (56).jpg',
+        headerText: 'Digital Disruption ',
+        subHeaderText: 'Infosys commissioned an independent survey of over 1,000 respondents at CXO or senior management level from organizations with annual revenues exceeding US$ 1 bn, and an employee base of over 5,000.',
+        knowMoreText: 'VIEW'
+    },
+    {
+        imageUrl: 'https://mdbootstrap.com/img/Photos/Others/images/83.jpg',
+        headerText: 'Innovation in Indiana',
+        subHeaderText: 'we have opened our flagship Technology & Innovation Hub in Indianapolis in March 2018, Infosys announced it is building its U.S. Education Center in the state, focused on developing the skills and capabilities required for our digital future.',
+        knowMoreText: 'VIEW'
+    }
+]
+
+export { homeMockData }
--- ReactSample/src/app/components/Home.js
+++ ReactSample/src/app/components/Home.js
@@ -1,12 +1,18 @@
 
 import React, { Component } from 'react'
 import { BlogComponent } from './Blog'
-
-
+import { Container, Row } from 'mdbreact';
+import { homeMockData } from '../MockData/HomeData'
 class Home extends Component {
   render() {
     return (
-      <BlogComponent />
+      <Container>
+        <section className="text-center my-5">
+          <h2 className="h1-responsive font-weight-bold my-5">Welocome to Infosys</h2>
+          <p className="grey-text w-responsive mx-auto mb-5">Infosys, a global leader in technology services & consulting, helps clients in more than 50 countries to create & execute digital transformation strategies.</p>
+            <BlogComponent blogResponse={homeMockData} />
+        </section>
+      </Container>
     )
   }
 }
