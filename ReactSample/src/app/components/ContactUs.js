7a4aae715009aaa7fa5ab6ed631ec4ad830d0c64
PUNAM\Home <punampawar23@gmail.com>
2018-08-26 22:31:44 +0530

    'contact'

--- ReactSample/src/app/components/ContactUs.js
+++ ReactSample/src/app/components/ContactUs.js
@@ -10,7 +10,7 @@ class ContactUsComponent extends Component {
             <Container>
                 <section className="my-5">
                     <h2 className="h1-responsive font-weight-bold text-center my-5">Contact us</h2>
-                    <p className="text-center w-responsive mx-auto pb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit, error amet numquam iure provident voluptate esse quasi, veritatis totam voluptas nostrum quisquam eum porro a pariatur veniam.</p>
+                    <p className="text-center w-responsive mx-auto pb-5">Please select your area of interest below. An Infosys representative will contact you shortly after receiving your request.For our contact details in a specific geography, please select the Infosys subsidiary.</p>
                     <Row>
                         <Col lg="5" className="lg-0 mb-4">
                             <FeedBackComponent />
